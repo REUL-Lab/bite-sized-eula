5d49f03d74b723d2c1a0414c604e86cb979038f6
michaelchen523 <michaelchen523@gmail.com>
2018-02-13 21:35:11 -0500

    popup works, styling per specifications

--- notification_popup.js
+++ notification_popup.js
@@ -1,11 +1,19 @@
 var text = document.getElementById('react-root');
-var string = "<div id='myModal' class='modal'>" +
-                "<div class='modal-content'>" +
-                  "<span class='close'>&times;</span>" +
-                    "<p>Some text in the Modal..</p>" +
-                "</div>" +
-            "</div>"
-
+var string = `
+<div id='myModal' class='modal'>
+    <div class='modal-content'>
+          <span class="close">&times;</span>
+          <h2 id="popTitle">Before you Sign Up</h2>
+          <div id="message">
+            <p>Instagram has the power to access everything you give them</p>
+          </div>
+          <div id="footer">
+              <span>Nevermind</span>
+              <span id="middle" class="align-right">Sign up</span>
+              <span class="align-right">Sign up and remind me next time</span>
+          </div>
+    </div>
+</div>`
 text.insertAdjacentHTML('beforeend', string);
 
 document.addEventListener('click', function(e) {
@@ -16,6 +24,7 @@ document.addEventListener('click', function(e) {
     var modal = document.getElementById('myModal');
     var span = document.getElementsByClassName("close")[0];
     var signUpElement = elements[1].childNodes[0];
+
     signUpElement.onclick = function() {
         modal.style.display="block";
     }
@@ -29,4 +38,5 @@ document.addEventListener('click', function(e) {
             modal.style.display = "none";
         }
     }
+
 }, false);
