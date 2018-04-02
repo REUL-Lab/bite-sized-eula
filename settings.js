d62085c66d251b61158fcb0d2bf7fd774ee692fc
Swati <swatimardia@gmail.com>
2018-04-02 05:50:44 -0400

    Changes to advanced settings page

--- settings.js
+++ settings.js
@@ -2,4 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('uninstall_link').addEventListener('click', function() {
         chrome.tabs.update({ url: 'chrome://extensions' });
     });
+
+    document.getElementById('donate_link').addEventListener('click', function() {
+        var x = document.getElementById('paypal');
+        var domain = document.getElementById('domains');
+        if (x.style.display === "none") {
+            domain.style.display = "none";
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+
+    });
+
+    document.getElementById('domain_link').addEventListener('click', function() {
+        var x = document.getElementById('domains');
+        var paypal = document.getElementById('paypal');
+        if (x.style.display === "none") {
+            paypal.style.display = "none";
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    });
 });
